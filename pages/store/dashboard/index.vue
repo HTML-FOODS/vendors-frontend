9a7bcee1d4ea8f51fa8f489476ac82a8e1f8b24a
emohex <olumidemm@gmail.com>
2022-09-06 06:21:01 +0100

    store switching availabity of meal

--- pages/store/dashboard/index.vue
+++ pages/store/dashboard/index.vue
@@ -13,7 +13,7 @@
         </v-col>
         <v-col cols="4">
           <v-card color="#C42D32" dark class="mx-auto" width="400">
-            <v-card-title class="text-h6">Today"s order</v-card-title>
+            <v-card-title class="text-h6">Today's order</v-card-title>
 
             <v-card-actions>
               <v-btn text> 5008</v-btn>
@@ -59,16 +59,29 @@
             </v-list-item-content>
           </v-list-item>
 
-          <v-card-actions>
+          <v-card-actions
+            v-for="(items, index) in item.vendorDetails.stores"
+            :key="index"
+          >
             <v-btn
               outlined
               rounded
               color="success"
               class="text-capitalize"
-              x-small
+              small
+              v-if="items.storeId == storeId"
             >
-              Available
+              {{ items.mealAvailable }}
             </v-btn>
+
+            <v-spacer />
+            <v-switch
+              flat
+              v-model="switch1"
+              inset
+              @change="changeAvailable(item._id)"
+              v-if="items.storeId == storeId"
+            ></v-switch>
           </v-card-actions>
         </v-card>
       </v-col>
@@ -83,105 +96,56 @@ export default {
   layout: 'storeDashboard',
   data() {
     return {
+      switch1: false,
       getVendormeals: '',
-      headers: [
-        {
-          text: 'Dessert (100g serving)',
-          align: 'start',
-          sortable: false,
-          value: 'name',
-        },
-        { text: 'Calories', value: 'calories' },
-        { text: 'Fat (g)', value: 'fat' },
-        { text: 'Carbs (g)', value: 'carbs' },
-        { text: 'Protein (g)', value: 'protein' },
-        { text: 'Iron (%)', value: 'iron' },
-      ],
-      desserts: [
-        {
-          name: 'Frozen Yogurt',
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: '1%',
-        },
-        {
-          name: 'Ice cream sandwich',
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: '1%',
-        },
-        {
-          name: 'Eclair',
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: '7%',
-        },
-        {
-          name: 'Cupcake',
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: '8%',
-        },
-        {
-          name: 'Gingerbread',
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: '16%',
-        },
-        {
-          name: 'Jelly bean',
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: '0%',
-        },
-        {
-          name: 'Lollipop',
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: '2%',
-        },
-        {
-          name: 'Honeycomb',
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: '45%',
-        },
-        {
-          name: 'Donut',
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: '22%',
-        },
-        {
-          name: 'KitKat',
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: '6%',
-        },
-      ],
+      currentState: true,
+      storeId: '',
     }
   },
   methods: {
+    async getstoreProfile() {
+      try {
+        const res = await this.$axios.$get(
+          `${this.$config.baseUrl}vendor/store/profile`
+        )
+        // console.log(res.payload[0].data[0].storeId)
+        this.storeId = res.payload[0].data[0].storeId
+      } catch (error) {
+        console.log(error.response)
+      }
+    },
+    async changeAvailable(mealId) {
+      console.log(this.switch1, mealId)
+      if (this.switch1 == false) {
+        console.log('not available', mealId)
+        try {
+          const res = await this.$axios.$post(
+            `${this.$config.baseUrl}vendor/store/meal/availability`,
+            {
+              _id: mealId,
+              setCondition: 'not available',
+            }
+          )
+          console.log(res)
+        } catch (error) {
+          console.log(error.response)
+        }
+      } else if (this.switch1 == true) {
+        console.log('available', mealId)
+        try {
+          const res = await this.$axios.$post(
+            `${this.$config.baseUrl}vendor/store/meal/availability`,
+            {
+              _id: mealId,
+              setCondition: 'available',
+            }
+          )
+          console.log(res)
+        } catch (error) {
+          console.log(error.response)
+        }
+      }
+    },
     async getMeals() {
       try {
         const res = await this.$axios.$get(
@@ -197,6 +161,7 @@ export default {
   },
   created() {
     this.getMeals()
+    this.getstoreProfile()
   },
 }
 </script>
